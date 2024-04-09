629e1edbdecc488ef01b2d8c985210347ba54781
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-04-09 22:51:22 +0530

    Added UserModel ans authHelper

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import colors from 'colors';
 import dotenv from "dotenv";
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js';
+
 // Configure env
 dotenv.config()
 
@@ -16,6 +18,10 @@ const app = express()
 // app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+
+// routes
+app.use('/api/v1/auth', authRoutes);
+
 
 app.get("/",(req,res)=>{
    res.send({
