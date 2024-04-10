c86e9e2746e85b2f7c2ab94671a1993da4df2399
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-04-10 21:26:14 +0530

    Login and Signup api routes added

---
+++ middleware/authmiddleware.js
@@ -0,0 +1,33 @@
+import JWT from "jsonwebtoken";
+import userModel from "../models/userModel.js";
+
+//Protected Routes token base
+export const requireSignIn = async (req, res, next) => {
+  try {
+    const decode = JWT.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+    req.user = decode;
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Admin access
+export const isAdmin = async (req, res, next) => {
+    try {
+        const user = await userModel.findById(req.user._id);
+        if (!user) {
+            return res.status(401).send({ success: false, message: "Unauthorized: User not found" });
+        }
+        if (user.role !== 1) {
+            return res.status(401).send({ success: false, message: "Unauthorized: Admin access required" });
+        }
+        next();
+    } catch (error) {
+        console.error("Error in isAdmin middleware:", error);
+        res.status(500).send({ success: false, message: "Internal Server Error" });
+    }
+};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,6 @@
 import express from 'express'
-import {registerController} from '../controller/authController.js'
+import {loginController, registerController, testController} from '../controller/authController.js'
+import { isAdmin, requireSignIn } from '../middleware/authmiddleware.js';
 
 //router object
 const router = express.Router();
@@ -7,5 +8,12 @@ const router = express.Router();
 //routing
 //REGISTER || METHOD POST
 router.post("/register", registerController);
+
+//LOGIN || POST
+router.post("/login", loginController);
+
+
+//test routes
+router.get("/test",requireSignIn,isAdmin, testController);
 
 export default router
