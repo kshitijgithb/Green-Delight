3d2a39305cbc28a2066bdb5d9ff643e272c49e48
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-04-12 22:58:25 +0530

    Create Category added in Admin Dashboard Page

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -82,7 +82,7 @@ export const singleCategoryController = async (req, res) => {
     const category = await categoryModel.findOne({ slug: req.params.slug });
     res.status(200).send({
       success: true,
-      message: "Get SIngle Category SUccessfully",
+      message: "Get Single Category Successfully",
       category,
     });
   } catch (error) {
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -27,6 +27,7 @@ router.get("/get-category", categoryControlller);
 //single category
 router.get("/single-category/:slug", singleCategoryController);
 
+
 //delete category
 router.delete(
   "/delete-category/:id",
