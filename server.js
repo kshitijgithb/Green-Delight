154fb9fc489792db1f8fd3a7d62767be72910217
kshitijgithb <kshitijkumargupta2017@gmail.com>
2024-04-18 23:08:49 +0530

    cors resolved

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ connectDB();
 const app = express()
 
 // middlewares
-app.use(cors())
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials:true,
+})
+);
 app.use(express.json())
 app.use(morgan('dev'))
 
